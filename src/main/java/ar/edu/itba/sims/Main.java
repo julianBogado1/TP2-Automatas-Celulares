55004590d80444919059b3f3097deb58dd0b58bd
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-08-14 05:29:58 -0300

    Nit

--- src/main/java/ar/edu/itba/sims/Main.java
+++ src/main/java/ar/edu/itba/sims/Main.java
@@ -28,33 +28,31 @@ public class Main {
             }
         }
 
-        for(int i=0; i < 150; i++) {
+        for (int i = 0; i < 150; i++) {
             // Only generate output every 1000 iterations
             // TODO: make this configurable (could be named step)
             final var animation_step = 5;
             if (i % animation_step == 0) {
                 StringBuilder sb = new StringBuilder();
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter(directoryPath + "/"+i/animation_step+".txt"))) {
-    
-                    for(Particle p : particles) {
+                try (BufferedWriter writer = new BufferedWriter(
+                        new FileWriter(directoryPath + "/" + i / animation_step + ".txt"))) {
+
+                    for (Particle p : particles) {
                         sb.append(p.getX()).append(" ")
-                          .append(p.getY()).append(" ")
-                          .append(p.getR()).append(" ")
-                          .append(p.getV()).append(" ")
-                          .append(p.getTheta()).append("\n");
+                                .append(p.getY()).append(" ")
+                                .append(p.getR()).append(" ")
+                                .append(p.getV()).append(" ")
+                                .append(p.getTheta()).append("\n");
                     }
                     writer.write(sb.toString());
-    
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
 
-            particles = nextFrame(particles, particles);    // TODO por ahora son todas vecinas
+            particles = nextFrame(particles, particles); // TODO por ahora son todas vecinas
         }
-
-
-
 
     }
 
@@ -62,14 +60,14 @@ public class Main {
 
         List<Particle> result = new ArrayList<>();
 
-        for(Particle p : particles) {
-            //POSITION
+        for (Particle p : particles) {
+            // POSITION
             Vector velocity = Vector.fromPolar(p.getV(), Math.toRadians(p.getTheta()));
             double newX = p.getX() + velocity.getX();
             double newY = p.getY() + velocity.getY();
 
-            //Check boundaries
-            //TODO this L is fixed currently
+            // Check boundaries
+            // TODO this L is fixed currently
             double L = 100.0;
             if (newX < 0 || newX > L) {
                 newX = (newX + L) % L; // Wrap around horizontally
@@ -78,7 +76,7 @@ public class Main {
                 newY = (newY + L) % L; // Wrap around vertically
             }
 
-            double noise = 2L; //noise amplitude
+            double noise = 2L; // noise amplitude
             double newTheta = p.computeAvgTheta(neighbors) + (Math.random() - 0.5) * noise;
             System.out.println("Old theta: " + p.getTheta() + ", New theta: " + newTheta);
 
