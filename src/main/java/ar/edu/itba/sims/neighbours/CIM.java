8f009db76d5637a45184c48315408e29b53756a7
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-08-15 08:19:23 -0300

    Improved CIM implementation

--- src/main/java/ar/edu/itba/sims/neighbours/CIM.java
+++ src/main/java/ar/edu/itba/sims/neighbours/CIM.java
@@ -13,6 +13,7 @@ public abstract class CIM {
      * Evaluates the interaction between particles in a simulation box.
      *
      * @apiNote Particles are assumed to be points (radius of 0).
+     * @apiNote Particles are neighbours to themselves.
      *
      * @param particles List of particles to evaluate
      * @param L         Length of the simulation box
@@ -24,21 +25,24 @@ public abstract class CIM {
         final var R2 = Rc * Rc;
 
         final var matrix = new Matrix<>(M, LinkedList<Particle>::new);
+        final var result = new HashMap<Particle, List<Particle>>();
 
         for (final var p : particles) {
             int i = (int) (p.getX() / Rc);
             int j = (int) (p.getY() / Rc);
             matrix.get(i, j).add(p);
-        }
 
-        final var result = new HashMap<Particle, List<Particle>>();
+            List<Particle> neighbours = new LinkedList<>();
+            neighbours.add(p);
+            result.putIfAbsent(p, neighbours);
+        }
 
         for (final var quadrant : matrix) {
             for (Particle p1 : quadrant) {
                 for (Particle p2 : quadrant) {
-                    if (p1.getId() < p2.getId() && p1.distance(p2, L) < R2) {
-                        result.computeIfAbsent(p1, k -> new LinkedList<>()).add(p2);
-                        result.computeIfAbsent(p2, k -> new LinkedList<>()).add(p1);
+                    if (p1.getId() < p2.getId() && p1.sqrdDistance(p2, L) < R2) {
+                        result.get(p1).add(p2);
+                        result.get(p2).add(p1);
                     }
                 }
             }
