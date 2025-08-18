bc1936717b9011f5d9d124e9e1d8a58e2a13d416
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-08-18 02:48:42 -0300

    Allow resuming simulations after finished

--- src/main/java/ar/edu/itba/sims/Main.java
+++ src/main/java/ar/edu/itba/sims/Main.java
@@ -6,24 +6,37 @@ import ar.edu.itba.sims.neighbours.CIM;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         InitialConditions ic = InitialStateParser.parse("initial_conditions.json", InitialConditions.class);
-        List<Particle> particles = InitialStateParser.buildInitialState(ic);
+
+        final int resume = Integer.valueOf(args.length > 0 ? args[0] : "0");
+
+        List<Particle> particles;
+        if (resume > 0) {
+            System.out.println("Resuming simulation from step " + resume);
+            // Retrieve the particles from the previous animation step
+            final var prev = resume / 5 /* Animation step */ - (resume % 5 == 0 ? 1 : 0);
+            particles = InitialStateParser.parseParticles(prev);
+        } else {
+            particles = InitialStateParser.buildInitialState(ic);
+        }
 
         double L = ic.getL();
         double Rc = ic.getR();
         double noise = ic.getNoise();
         int steps = ic.getSteps();
         double v = ic.getV();
-        simulate(L, Rc, noise, steps, v, particles);
+        simulate(L, Rc, noise, steps, v, particles, resume);
     }
 
     public static List<Particle> nextFrame(Map<Particle, List<Particle>> particles_neighbors, double L, double noise,
@@ -65,22 +78,37 @@ public class Main {
         }
     }
 
-    public static void simulate(double L, double Rc, double noise, int steps, double v, List<Particle> particles)
+    public static void simulate(double L, double Rc, double noise, int steps, double v, List<Particle> particles, int resume)
             throws IOException {
         final var executor = Executors.newFixedThreadPool(3);
 
         final var directoryPath = "src/main/resources/time_slices";
-        preparePath(directoryPath, false);
+        preparePath(directoryPath, resume != 0);
 
         final var orderPath = "src/main/resources/order_parameter";
         preparePath(orderPath, true);
 
         final var filename = "%s/N-%d L-%.2f Ruido-%.2f.txt".formatted(orderPath, particles.size(), L, noise);
-        final var orderWriter = new BufferedWriter(new FileWriter(filename));
+
+        // Truncate the file to resume
+        try (final var stream = new FileOutputStream(filename, true)) {
+            stream.getChannel().truncate(resume * 19L); // Each number is 19 chars long
+        } catch (IOException e) {
+            System.err.println("Error resuming simulation: " + e.getMessage());
+            return;
+        }
+
+        final var orderWriter = new BufferedWriter(new FileWriter(filename, true));
+
+        // Resuming with the info from the previous step
+        if (resume > 0) {
+            final var particles_neighbors = CIM.evaluate(particles, L, Rc);
+            particles = nextFrame(particles_neighbors, L, noise, v);
+        }
 
         final var animation_step = 5;
         final var progress_step = steps / 10;
-        for (int i = 0; i < steps; i++) {
+        for (int i = resume; i < steps; i++) {
             if (i % animation_step == 0) {
                 executor.submit(new Animator(i / animation_step, v, particles));
             }
