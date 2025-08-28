41ba0d8ab86bf8c41591e1febebaf84258c0aefc
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-08-28 15:37:56 -0300

    Use scientific notation on ticks

--- src/main/python/orders.py
+++ src/main/python/orders.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
+import numpy as np
 
 import os
 
@@ -23,7 +26,22 @@ for filename in sources:
     y_axis = list(range(0, len(numbers) * 5, 5))
     ax.plot(y_axis, numbers)
 
-ax.ticklabel_format(useOffset=False, style='plain')
+def sci_notation(val, _):
+    if val == 0:
+        return rf"$0\times 10^{{0}}$"
+    exponent = int(np.floor(np.log10(abs(val))))
+    coeff = val / 10**exponent
+    # if int(coeff) == 1:
+    #     return rf"$10^{{{exponent}}}$"
+    return rf"${coeff:.0f}\times 10^{{{exponent}}}$"
+
+ax.xaxis.set_major_formatter(FuncFormatter(sci_notation))
+ax.yaxis.set_major_formatter(FuncFormatter(sci_notation))
+
+# ax.ticklabel_format(useOffset=False, style='plain')
+
+# ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+# ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 
 plt.xlabel('Pasos', fontsize=20)
 plt.ylabel('Orden', fontsize=20)
